1eb45c6260294e024e5e5271eec51c9bbf28d1b4
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-08-06 23:34:16 +0300

    #283

---
+++ src/easy/#283. Move Zeroes.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/move-zeroes/description/
+
+function moveZeroes(arr) {
+  let left = 0; // всегда будет указывать на самый крайне левый 0
+  let right = 0; // всегда идет направо пока не найдет число отличное от нуля
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[right] !== 0) {
+      let old = arr[right];
+      arr[right] = arr[left];
+      arr[left] = old;
+      left++;
+    }
+
+    right++;
+  }
+
+  return arr;
+}
