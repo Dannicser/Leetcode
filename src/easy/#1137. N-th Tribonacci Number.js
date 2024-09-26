d496b22dea740051a04d20e11a2a03369f66bcce
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-09-26 23:34:28 +0300

    ref: #1137, #1346

--- src/easy/#1137. N-th Tribonacci Number.js
+++ src/easy/#1137. N-th Tribonacci Number.js
@@ -9,3 +9,22 @@ function tribonacci(n) {
 
   return db[db.length - 1];
 }
+
+function tribonacci(n) {
+  const dp = [0, 1, 1];
+
+  if (n < 3) {
+    return dp[n];
+  }
+
+  let i = 0;
+  let sum = 0;
+
+  while (i + 2 < n) {
+    sum = dp[i] + dp[i + 1] + dp[i + 2];
+    dp.push(sum);
+    i++;
+  }
+
+  return sum;
+}
--- src/easy/#1346. Check If N and Its Double Exist.js
+++ src/easy/#1346. Check If N and Its Double Exist.js
@@ -14,3 +14,19 @@ function checkIfExist(arr) {
 
   return false;
 }
+
+function checkIfExist(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) {
+        continue;
+      }
+
+      if (arr[i] === arr[j] * 2 || arr[i] === arr[j] / 2) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
